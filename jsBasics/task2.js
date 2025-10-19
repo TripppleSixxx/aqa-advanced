e41866b27ff9e5ccd9e2d23e0cec10b02927cf6b
Viacheslav <boreesenko@gmail.com>
2025-10-19 15:51:56 +0300

    My homeworks for four lesson.

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+let maleName = "John";
+let femaleName = "Alice";
+let greeting = "Hello, " + maleName + " and " + femaleName + ", nice to meet you!";
+
+console.log(greeting);
+
+let greetingTemplete = `Also, ${maleName} and ${femaleName}, welcome to JavaScript!`;
+
+console.log(greetingTemplete);
---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+let personeAge = 17;
+let isAdult = personeAge >= 18;
+
+console.log("John is " + personeAge + " years old");
+console.log("Is he an adult? - " + isAdult);
