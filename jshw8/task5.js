f346bdc5bf0df4e8493329e03ea0e19079115841
Viacheslav <boreesenko@gmail.com>
2025-11-12 21:28:01 +0200

    My homeworks for lesson eight.

---
+++ jshw8/task5.js
@@ -0,0 +1,6 @@
+//Завдання 5: Об'єднання масивів
+
+const firstArray = [1, 2, 3, 4, 5];
+const secondArray = [6, 7, 8, 9, 10];
+const concatenatedArray = firstArray.concat(secondArray);
+console.log(concatenatedArray);
